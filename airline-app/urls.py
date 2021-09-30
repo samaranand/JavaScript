7847c2a8b28e7cf23bab3be058dc09dbe108f06c
Pratyaydeep Ghanta <pratyaydeepghanta@gmail.com>
2021-10-01 00:51:39 +0530

    Add files via upload

---
+++ airline-app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    path("pratyaydeep", views.pratyaydeep, name="pratyaydeep")
+]
